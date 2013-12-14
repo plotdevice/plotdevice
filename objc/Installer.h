4acde401ecae9c7d62099bc2046ffdb1021b53a2
Christian Swinehart <drafting@samizdat.cc>
2013-12-13 22:03:41 -0800

    a snippet to authenticate before symlinking to console.py

---
+++ objc/Installer.h
@@ -0,0 +1,13 @@
+//
+//  Installer.h
+//  NodeBox
+//
+//  Created by Christian Swinehart on 12/13/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Installer : NSObject
++ (BOOL)createLink:(NSString *)toolPath;
+@end
---
+++ objc/Installer.m
@@ -0,0 +1,34 @@
+//
+//  Installer.m
+//  NodeBox
+//
+//  Created by Christian Swinehart on 12/13/13.
+//
+//
+
+#import "Installer.h"
+
+@implementation Installer
+
++ (BOOL)createLink:(NSString *)toolPath{
+    NSString *toolDir = [toolPath stringByDeletingLastPathComponent];
+    NSString *bundle_path = [[NSBundle mainBundle ] bundlePath];
+    NSString *console_py = [bundle_path stringByAppendingString:@"/Contents/Resources/python/nodebox/console.py"];
+    OSStatus status;
+    FILE *pipe = NULL;
+        
+    char *src = (char *)[console_py UTF8String];
+    char *dst = (char *)[toolPath UTF8String];
+    char *dstdir = (char *)[toolDir UTF8String];
+    char *mkdir = "/bin/mkdir";
+    char *mkdirArgs[] = {"-p", dstdir, NULL};
+    char *link = "/bin/ln";
+    char *linkArgs[] = {"-s", "-f", "-F", "-h", src, dst, NULL};
+
+    AuthorizationRef ref;
+    AuthorizationFlags flags = kAuthorizationFlagDefaults;
+    status = AuthorizationCreate(NULL, kAuthorizationEmptyEnvironment, flags, &ref);
+    status = AuthorizationExecuteWithPrivileges(ref, mkdir, flags, mkdirArgs, &pipe);
+    status = AuthorizationExecuteWithPrivileges(ref, link, flags, linkArgs, &pipe);
+}
+@end
