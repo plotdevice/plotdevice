e8556c84fd75baa7266a8072072b0a02a6d44c08
Christian Swinehart <drafting@samizdat.cc>
2014-01-05 23:22:19 -0500

    further simplification of boot scripts

--- boot/main.m
+++ boot/main.m
@@ -4,23 +4,12 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        NSBundle *mainBundle = [NSBundle mainBundle];
-        NSString *mainFilePath = [mainBundle pathForResource: @"nodebox-app" ofType:@"py"];
-        if ( !mainFilePath ) {
-            [NSException raise: NSInternalInconsistencyException format: @"%s:%d main() Failed to find the nodebox-app.py file in the application wrapper's Resources directory.", __FILE__, __LINE__];
-        }
-        
         Py_SetProgramName("/usr/bin/python");
         Py_Initialize();
         PySys_SetArgv(argc, (char **)argv);
-        
+        NSString *mainFilePath = [[NSBundle mainBundle] pathForResource:@"nodebox-app" ofType:@"py"];
         const char *mainFilePathPtr = [mainFilePath UTF8String];
         FILE *mainFile = fopen(mainFilePathPtr, "r");
-        int result = PyRun_SimpleFile(mainFile, (char *)[[mainFilePath lastPathComponent] UTF8String]);
-        
-//        if ( result != 0 )
-//        [NSException raise: NSInternalInconsistencyException
-//                    format: @"%s:%d main() PyRun_SimpleFile failed with file '%@'.  See console for errors.", __FILE__, __LINE__, mainFilePath];
-        return result;
+        return PyRun_SimpleFile(mainFile, (char *)[[mainFilePath lastPathComponent] UTF8String]);
     }
 }
--- nodebox/gui/__init__.py
+++ nodebox/gui/__init__.py
@@ -1,6 +1,3 @@
-import objc
-objc.setVerbose(1)
-
 from nodebox.gui.document import NodeBoxDocument, FullscreenView
 from nodebox.gui.app import NodeBoxAppDelegate
 from nodebox.gui.views import ZoomPanel, NodeBoxBackdrop, NodeBoxGraphicsView
