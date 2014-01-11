67f9552eed5efe881e6934dc602c40e50acb996b
Christian Swinehart <drafting@samizdat.cc>
2014-01-10 22:45:48 -0500

    added context managers for pdf pages & image sequences

--- nodebox/script.py
+++ nodebox/script.py
@@ -20,10 +20,11 @@ if nodebox.app:
     # become a no-op if imported inside the NodeBox app or command line tool
     __all__ = [] 
 else:
-    import time, sys
+    import time, sys, re
     from nodebox import graphics
     from nodebox import util
     from contextlib import contextmanager
+    from Quartz.PDFKit import *
 
     # Trivial subclass of graphics.Canvas that also resets the context when `clear` is called
     class Canvas(graphics.Canvas):
@@ -81,10 +82,18 @@ else:
         def frame(self):
             """Clears the canvas, runs the code in the `with` block, then adds the canvas to the movie.
 
-            For example, to create a quicktime movie and write a single frame to it:
+            For example, to create a quicktime movie and write a single frame to it you could write:
+
+                with export("anim.mov") as movie:
+                    canvas.clear()
+                    ... # draw the frame
+                    movie.add()
+
+            With the `frame` context manager, this simplifies to:
+
                 with export("anim.mov") as movie:
                     with movie.frame:
-                        rect(10,10,100,100)
+                        ... # draw the frame
             """
             canvas.clear()
             yield
@@ -114,13 +123,134 @@ else:
             sys.stderr.flush()
 
     # Context manager returned by `export` when an image-type file extension is provided
-    class Image(object):
+    class ImageSequence(object):
+        """Write a single image, or a numbered sequence of them.
+
+        To save a single image:
+
+            with export('output.png') as image:
+                ... # draw something
+
+        To draw a sequence of images, you can either handle the naming yourself:
+
+            for i in xrange(100):
+                with export('output-%03i.pdf'%i) as image:
+                    ... # draw the next image in the sequence
+
+        Or you can let the `sequence` context manager number them for you:
+
+            with export('output.jpg') as image:
+                for i in xrange(100):
+                    with image.sequence:
+                        ... # draw the next image in the sequence                    
+
+        """
         def __init__(self, fname, format):
             self.fname = fname
+            self.format = format
+            self.idx = None
+            if '#' in fname:
+                head, tail = re.split(r'#+', fname, maxsplit=1)
+                counter = '%%0%ii' % (len(fname) - len(head) - len(tail))
+                self.tmpl = "".join([head,counter,tail.replace('#','')])
+            else:
+                self.tmpl = re.sub(r'^(.*)(\.[a-z]{3,4})$', r'\1-%04i\2', fname)
         def __enter__(self):
-          canvas.clear()
+            canvas.clear()
+            return self
         def __exit__(self, type, value, tb):
-          canvas.save(fname, format)
+            if self.idx is None:
+                canvas.save(self.fname, self.format)
+        @property
+        @contextmanager
+        def sequence(self):
+            """Clears the canvas, runs the code in the `with` block, then saves a numbered output file.
+
+            For example, to a sequence of 10 images:
+                with export('output.png') as image:
+                    for i in xrange(100):
+                        with image.sequence:
+                            ... # draw the next image in the sequence
+            """
+            canvas.clear()
+            yield
+            if self.idx is None:
+                self.idx = 1
+            canvas.save(self.tmpl%self.idx, self.format)
+            self.idx += 1
+
+    # Context manager returned by `export` for PDF files (allowing single or multi-page docs)
+    class PDF(object):
+        """Represents a PDF document in the process of being assembled one page at a time.
+
+        The class can be used procedurally to add frames and finish writing the output file:
+
+            pdf = export('multipage.pdf')
+            for i in xrange(5):
+                canvas.clear() # erase the previous page's graphics from the canvas
+                ...            # (do some drawing)
+                pdf.add()      # add the canvas to the pdf as a new page
+            pdf.finish()       # write the pdf document to disk
+
+        It can be used more simply as a context manager:
+
+            with export('multipage.pdf') as pdf:
+                for i in xrange(5):
+                    with pdf.page:
+                        ... # draw the next page
+
+            with export('singlepage.pdf') as pdf:
+                ... # draw the one and only page
+        """
+        def __init__(self, fname):
+            self.fname = fname
+            self.doc = None
+        def __enter__(self):
+            canvas.clear()
+            return self
+        def __exit__(self, type, value, tb):
+            self.finish() or canvas.save(self.fname, 'pdf')                
+
+        @property
+        @contextmanager
+        def page(self):
+            """Clears the canvas, runs the code in the `with` block, then adds the canvas as a new pdf page.
+
+            For example, to create a pdf with two pages, you could write:
+
+                with export("multipage.pdf") as pdf:
+                    canvas.clear()
+                    ... # draw first page
+                    pdf.add()
+                    canvas.clear()
+                    ... # draw the next page
+                    pdf.add()
+
+            With the `page` context manager it simplifies to:
+
+                with export("multipage.pdf") as pdf:
+                    with pdf.page:
+                        ... # draw first page
+                    with pdf.page:
+                        ... # draw the next page
+            """
+            canvas.clear()
+            yield
+            self.add()            
+
+        def add(self):
+            """Add a new page to the PDF with the current contents of the canvas."""
+            pagedoc = PDFDocument.alloc().initWithData_(canvas._getImageData('pdf'))
+            if not self.doc:
+                self.doc = pagedoc
+            else:
+                self.doc.insertPage_atIndex_(pagedoc.pageAtIndex_(0), self.doc.pageCount())
+
+        def finish(self):
+            """Writes the fully-assembled PDF to disk"""
+            if self.doc:
+                self.doc.writeToFile_(self.fname)
+            return self.doc
 
     def export(fname, fps=None, loop=None, bitrate=1.0):
         """Context manager for image/animation batch exports.
@@ -149,17 +279,29 @@ else:
 
         Note that the `loop` argument only applies to animated gifs and `bitrate` is used in the H.264 
         encoding of `mov` files.
+
+        For implementational details, inspect the format-specific exporters in the repl:
+            help(export.PDF)
+            help(export.Movie)
+            help(export.ImageSequence)
         """
         format = fname.rsplit('.',1)[1]
         if format=='mov' or (format=='gif' and fps or loop is not None):
             fps = fps or 30 # set a default for .mov exports
             loop = {True:-1, False:0, None:0}.get(loop, loop) # convert bool args to int
             return Movie(fname, format, fps=fps, bitrate=bitrate, loop=loop)
-        elif format in ('pdf','eps','png','jpg','gif','tiff'):
-            return Image(fname, format)
+        elif format=='pdf':
+            return PDF(fname)
+        elif format in ('eps','png','jpg','gif','tiff'):
+            return ImageSequence(fname, format)
         else:
             unknown = 'Unknown export format "%s"'%format
             raise RuntimeError(unknown)
+
+    # add references to the export context managers (so users can call help() on them)
+    export.PDF = PDF
+    export.Movie = Movie
+    export.ImageSequence = ImageSequence
 
     # create a canvas and graphics context for the draw functions to operate on
     ns = {"export":export}
