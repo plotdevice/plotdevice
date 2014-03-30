adca04d7f9b2e6f44a1e22312fc49270e6732b43
Christian Swinehart <drafting@samizdat.cc>
2014-03-29 20:18:54 -0400

    carmack weeps
    - whatever speedup fast_inv_sqrt once provided hardly feels worth the
    trade-off of stepping to single precision floats

--- libs/geometry/cGeometry.c
+++ libs/geometry/cGeometry.c
@@ -3,20 +3,26 @@
 
 // FAST INVERSE SQRT
 // Chris Lomont, http://www.math.purdue.edu/~clomont/Math/Papers/2003/InvSqrt.pdf
-float _fast_inverse_sqrt(float x) { 
-    float xhalf = 0.5f*x; 
+float _fast_inverse_sqrt(float x) {
+    float xhalf = 0.5f*x;
     int i = *(int*)&x;
     i = 0x5f3759df - (i>>1);
     x = *(float*)&i;
     x = x*(1.5f-xhalf*x*x);
-    return x; 
+    return x;
 }
+
+// we're not running doom on a 32 bit cpu anymore...
+double _correct_inverse_sqrt(double x){
+    return 1.0/sqrt(x);
+}
+
 static PyObject *
 fast_inverse_sqrt(PyObject *self, PyObject *args) {
-    double x;   
+    double x;
     if (!PyArg_ParseTuple(args, "d", &x))
         return NULL;
-    x = _fast_inverse_sqrt(x);
+    x = _correct_inverse_sqrt(x);
     return Py_BuildValue("d", x);
 }
 
@@ -26,7 +32,7 @@ void _angle(double x0, double y0, double x1, double y1, double *a) {
 }
 static PyObject *
 angle(PyObject *self, PyObject *args) {
-    double x0, y0, x1, y1, a;    
+    double x0, y0, x1, y1, a;
     if (!PyArg_ParseTuple(args, "dddd", &x0, &y0, &x1, &y1))
         return NULL;
     _angle(x0, y0, x1, y1, &a);
@@ -35,11 +41,11 @@ angle(PyObject *self, PyObject *args) {
 
 // DISTANCE
 void _distance(double x0, double y0, double x1, double y1, double *d) {
-    *d = 1.0 / _fast_inverse_sqrt((x1-x0)*(x1-x0) + (y1-y0)*(y1-y0));
+    *d = 1.0 / _correct_inverse_sqrt((x1-x0)*(x1-x0) + (y1-y0)*(y1-y0));
 }
 static PyObject *
 distance(PyObject *self, PyObject *args) {
-    double x0, y0, x1, y1, d;   
+    double x0, y0, x1, y1, d;
     if (!PyArg_ParseTuple(args, "dddd", &x0, &y0, &x1, &y1))
         return NULL;
     _distance(x0, y0, x1, y1, &d);
@@ -53,22 +59,22 @@ void _coordinates(double x0, double y0, double d, double a, double *x1, double *
 }
 static PyObject *
 coordinates(PyObject *self, PyObject *args) {
-    double x0, y0, d, a, x1, y1;   
+    double x0, y0, d, a, x1, y1;
     if (!PyArg_ParseTuple(args, "dddd", &x0, &y0, &d, &a))
         return NULL;
     _coordinates(x0, y0, d, a, &x1, &y1);
     return Py_BuildValue("dd", x1, y1);
 }
 
-static PyMethodDef geometry_methods[]={ 
+static PyMethodDef geometry_methods[]={
     { "fast_inverse_sqrt", fast_inverse_sqrt, METH_VARARGS },
-    { "angle", angle, METH_VARARGS }, 
-    { "distance", distance, METH_VARARGS }, 
-    { "coordinates", coordinates, METH_VARARGS },  
+    { "angle", angle, METH_VARARGS },
+    { "distance", distance, METH_VARARGS },
+    { "coordinates", coordinates, METH_VARARGS },
     { NULL, NULL }
 };
 
-PyMODINIT_FUNC initcGeometry(void){ 
+PyMODINIT_FUNC initcGeometry(void){
     PyObject *m;
     m = Py_InitModule("cGeometry", geometry_methods);
 }
