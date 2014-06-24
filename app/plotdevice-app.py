2f91f5b0f6d2e51f470f7af161fefb4ba35a367a
Christian Swinehart <drafting@samizdat.cc>
2014-06-24 19:34:12 -0400

    ctrl-c will quit the app if run from terminal

--- app/plotdevice-app.py
+++ app/plotdevice-app.py
@@ -2,16 +2,20 @@ import sys
 import objc
 import Foundation
 import AppKit
+from signal import signal, SIGINT
 from os.path import dirname, abspath, join
 from PyObjCTools import AppHelper
 
-# rather than hijacking PYTHONPATH in the .m loader, add the module + .so directory here
-# (we insert rather than append to avoid getting trapped in py2app's zipfile)
-sys.path.insert(0, '%s/Contents/Resources/python'%abspath(Foundation.NSBundle.mainBundle().bundlePath()))
+# rather than hijacking PYTHONPATH in the .m loader, add the module directory now
+sys.path.append('%s/Contents/Resources/python'%abspath(Foundation.NSBundle.mainBundle().bundlePath()))
+
+# install a signal handler to quit on ^c (should the app ever be launched from terminal)
+signal(SIGINT, lambda m,n: AppKit.NSApplication.sharedApplication().terminate_(True))
 
 # PlotDevice is a typical document-based application. We'll import the PlotDeviceDocument
 # class et al from the gui module and the corresponding document-type defined in the
 # info.plist will do the rest.
 import plotdevice.gui
 
+# loop forever
 AppHelper.runEventLoop()
