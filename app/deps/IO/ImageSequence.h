3a7f4e09c406b120befc4b06b65280f681e61b32
Christian Swinehart <drafting@samizdat.cc>
2014-06-30 18:02:37 -0400

    image sequences now support multi-page pdfs

--- app/deps/IO/ImageSequence.h
+++ app/deps/IO/ImageSequence.h
@@ -7,15 +7,27 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Quartz/Quartz.h>
 
 @interface ImageSequence : NSObject{
     NSOperationQueue *queue;
     NSInteger framesWritten;
+    PDFDocument *pages;
+    NSInteger pageCount;
+    BOOL paginated;
+    NSString *filePath;
+    BOOL doneWriting;
 }
 @property (nonatomic, assign) NSInteger framesWritten;
+@property (nonatomic, assign) NSInteger pageCount;
+@property (nonatomic, assign) BOOL paginated;
+@property (nonatomic, retain) PDFDocument *pages;
+@property (nonatomic, copy) NSString *filePath;
+@property (assign) BOOL doneWriting;
 
-- (id)init;
-- (void)writeData:(NSData *)img toFile:(NSString *)fname;
+- (id)initWithFile:(NSString *)fname paginated:(BOOL)isMultipage;
+// - (void)writeData:(NSData *)img toFile:(NSString *)fname;
+- (void)closeFile;
 - (void)_wroteFrame;
 
 @end
--- app/deps/IO/ImageSequence.m
+++ app/deps/IO/ImageSequence.m
@@ -8,6 +8,9 @@
 
 #import "ImageSequence.h"
 
+//
+// batch imagefile writer
+//
 @interface ImageWriter : NSOperation{
     ImageSequence *delegate;
     NSString *fname;
@@ -30,28 +33,111 @@
 }
 @end
 
+//
+// multipage pdf writer
+//
+@interface PaperbackWriter : NSOperation{
+    ImageSequence *delegate;
+    PDFDocument *book;
+    NSData *page;
+    NSString *destination;
+}
+@property (nonatomic, assign) ImageSequence *delegate;
+@property (nonatomic, retain) PDFDocument *book;
+@property (nonatomic, retain) NSData *page;
+@property (nonatomic, retain) NSString *destination;
+@end
+
+@implementation PaperbackWriter
+@synthesize delegate, book, page, destination;
+-(void)main{
+    @autoreleasepool{
+        if (self.destination){
+            [self.book writeToFile:self.destination];
+            return;
+        }
+        PDFDocument *pageDoc = [[PDFDocument alloc] initWithData:self.page];
+        [self.book insertPage:[pageDoc pageAtIndex:0] atIndex:[self.book pageCount]];
+        [self.delegate performSelectorOnMainThread:@selector(_wroteFrame) withObject:nil waitUntilDone:NO];
+    }
+}
+
+- (void)dealloc{
+    self.book = nil;
+    self.page = nil;
+    self.destination = nil;
+    [super dealloc];
+}
+
+@end
 
 @implementation ImageSequence
-@synthesize framesWritten;
-- (id)init{
+@synthesize framesWritten, doneWriting, filePath, paginated, pages, pageCount;
+- (id)initWithFile:(NSString *)fname paginated:(BOOL)isMultipage{
 	if ((self = [super init])) {
-		self.framesWritten = 0;
-		queue = [[NSOperationQueue alloc] init];
-		queue.maxConcurrentOperationCount = 3;
+		self.framesWritten = self.pageCount = 0;
+        self.paginated = isMultipage;
+        queue = [[NSOperationQueue alloc] init];
+
+        if (isMultipage){
+            self.filePath = fname;
+            queue.maxConcurrentOperationCount = 1;
+        }else{
+            NSString *ext = [fname pathExtension];
+            NSString *basename = [fname stringByDeletingPathExtension];
+            NSString *seq = @"%05d";
+            self.filePath = [NSString stringWithFormat:@"%@-%@.%@", basename, seq, ext];
+            queue.maxConcurrentOperationCount = 3;
+        }
+
 	}
 	return self;
 }
 
-- (void)writeData:(NSData *)img toFile:(NSString *)fname{
-    ImageWriter *iw = [[[ImageWriter alloc] init] autorelease];
-    iw.delegate = self;
-    iw.fname = fname;
-    iw.image = img;
-    [queue addOperation:iw];
+- (void)addPage:(NSData *)img{
+    if (self.paginated){
+        if (!self.pages){
+            self.pages = [[PDFDocument alloc] initWithData:img];
+            [self _wroteFrame];
+        }else{
+            PaperbackWriter *pw = [[[PaperbackWriter alloc] init] autorelease];
+            pw.delegate = self;
+            pw.book = self.pages;
+            pw.page = img;
+            [queue addOperation:pw];
+        }
+    }else{
+        ImageWriter *iw = [[[ImageWriter alloc] init] autorelease];
+        iw.delegate = self;
+        iw.fname = [NSString stringWithFormat:self.filePath, ++self.pageCount];;
+        iw.image = img;
+        [queue addOperation:iw];
+    }
+}
+
+- (void)closeFile{
+    if (!self.paginated) return;
+
+    PaperbackWriter *pw = [[[PaperbackWriter alloc] init] autorelease];
+    pw.delegate = self;
+    pw.book = self.pages;
+    pw.page = nil;
+    pw.destination = self.filePath;
+    [queue addOperation:pw];
+
+    NSInvocationOperation *done = [[NSInvocationOperation alloc] initWithTarget:self
+                                                                        selector:@selector(_wroteAll)
+                                                                          object:nil];
+    [done addDependency:pw];
+    [queue addOperation:done];
 }
 
 - (void)_wroteFrame{
     self.framesWritten++;
+}
+
+- (void)_wroteAll{
+    self.doneWriting = YES;
 }
 
 - (void)dealloc{
