8f3944e5c1fd84af2997f30a584ebb07064c2a6f
Christian Swinehart <drafting@samizdat.cc>
2014-11-03 18:51:18 -0500

    CGPath converter was auto-closing *all* paths

--- app/deps/pathmatics/pathmatics.m
+++ app/deps/pathmatics/pathmatics.m
@@ -689,7 +689,6 @@ int xmain(int argc, char *argv[])
     if (numElements > 0){
         CGMutablePathRef    path = CGPathCreateMutable();
         NSPoint             points[3];
-        BOOL                didClosePath = YES;
 
         for (i = 0; i < numElements; i++){
             switch ([nsPath elementAtIndex:i associatedPoints:points]){
@@ -699,26 +698,19 @@ int xmain(int argc, char *argv[])
 
                 case NSLineToBezierPathElement:
                     CGPathAddLineToPoint(path, NULL, points[0].x, points[0].y);
-                    didClosePath = NO;
                     break;
 
                 case NSCurveToBezierPathElement:
                     CGPathAddCurveToPoint(path, NULL, points[0].x, points[0].y,
                                         points[1].x, points[1].y,
                                         points[2].x, points[2].y);
-                    didClosePath = NO;
                     break;
 
                 case NSClosePathBezierPathElement:
                     CGPathCloseSubpath(path);
-                    didClosePath = YES;
                     break;
             }
         }
-
-        // Be sure the path is closed or Quartz may not do valid hit detection.
-        if (!didClosePath)
-            CGPathCloseSubpath(path);
 
         immutablePath = CGPathCreateCopy(path);
         CGPathRelease(path);
