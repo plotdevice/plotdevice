c748136a9f3e9f67f9c968bafc531f57b8c0e660
Christian Swinehart <drafting@samizdat.cc>
2014-11-09 01:47:42 -0500

    arg-fiddling

--- app/deps/IO/setup.py
+++ app/deps/IO/setup.py
@@ -3,7 +3,8 @@ from setuptools.extension import Extension
 
 sources = ['module.m', 'SysAdmin.m', 'Pages.m', 'AnimatedGif.m', 'Video.m']
 frameworks = ['AppKit', 'Foundation', 'Quartz', 'Security', 'AVFoundation', 'CoreMedia', 'CoreVideo']
-cIO = Extension('cIO', sources=sources, extra_link_args=sum((['-framework', fmwk] for fmwk in frameworks), []))
+flags = sum((['-framework', fmwk] for fmwk in frameworks), [])
+cIO = Extension('cIO', sources=sources, extra_link_args=flags)
 
 setup (name = "cIO",
        version = "1.0",
