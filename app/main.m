f7665dd3fd358397491838f7b0d710cea15d11cd
Christian Swinehart <drafting@samizdat.cc>
2015-05-27 16:46:34 -0400

    updated .app bootstrapping
    - now finds the Resources/python directory through path-walking rather
    than NSBundle (since PyObjC hasn’t been loaded yet)

--- app/main.m
+++ app/main.m
@@ -7,7 +7,6 @@ int main(int argc, char *argv[])
         Py_Initialize();
         PySys_SetArgv(argc, (char **)argv);
         NSString *mainFilePath = [[NSBundle mainBundle] pathForResource:@"plotdevice-app" ofType:@"py"];
-        NSString *mainFileName = [mainFilePath lastPathComponent];
-        return PyRun_SimpleFile(fopen([mainFilePath UTF8String], "r"), (char *)[mainFileName UTF8String]);
+        return PyRun_SimpleFile(fopen([mainFilePath UTF8String], "r"), (char *)[mainFilePath UTF8String]);
     }
 }
--- app/plotdevice-app.py
+++ app/plotdevice-app.py
@@ -1,21 +1,19 @@
 import sys
-import objc
-import Foundation
-import AppKit
-from signal import signal, SIGINT
 from os.path import dirname, abspath, join
-from PyObjCTools import AppHelper
 
 # rather than hijacking PYTHONPATH in the .m loader, add the module directory now
-sys.path.append('%s/Contents/Resources/python'%abspath(Foundation.NSBundle.mainBundle().bundlePath()))
-
-# install a signal handler to quit on ^c (should the app ever be launched from terminal)
-signal(SIGINT, lambda m,n: AppKit.NSApplication.sharedApplication().terminate_(True))
+sys.path.append(join(dirname(abspath(__file__)), 'python'))
 
 # PlotDevice is a typical document-based application. We'll import the PlotDeviceDocument
 # class et al from the gui module and the corresponding document-type defined in the
 # info.plist will do the rest.
 import plotdevice.gui
 
+# install a signal handler to quit on ^c (should the app ever be launched from terminal)
+import AppKit
+from signal import signal, SIGINT
+signal(SIGINT, lambda m,n: AppKit.NSApplication.sharedApplication().terminate_(True))
+
 # loop forever
+from PyObjCTools import AppHelper
 AppHelper.runEventLoop()
