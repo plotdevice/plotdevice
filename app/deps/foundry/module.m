4406569d8108e39e60a048f24a208ad552e9f715
Christian Swinehart <drafting@samizdat.cc>
2015-06-29 21:37:45 -0400

    updated c-extensions 1/2

--- app/deps/foundry/module.m
+++ app/deps/foundry/module.m
@@ -1,11 +1,12 @@
 #import <Python.h>
+#include "../compat.h"
 
 PyMethodDef methods[] = {
   {NULL, NULL},
 };
 
-void initcFoundry()
-  {
-    (void)Py_InitModule("cFoundry", methods);
-  }
-
+MOD_INIT(cFoundry){
+    PyObject *m;
+    MOD_DEF(m, "cFoundry", "Dept. of Typography & Tracing", methods)
+    return MOD_SUCCESS_VAL(m);
+}
