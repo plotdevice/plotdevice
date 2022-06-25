eade22ebe9e479b86601a048366aacf839beeeb3
Christian Swinehart <drafting@samizdat.co>
2022-06-25 15:12:26 -0400

    add heic support to export()

--- plotdevice/context.py
+++ plotdevice/context.py
@@ -1495,8 +1495,8 @@ class Context(object):
         """
 
         # determine the format by normalizing the file extension
-        if format not in ('pdf','eps','png','jpg','gif','tiff', 'mov'):
         format = fname.lower().rsplit('.',1)[-1]
+        if format not in ('pdf','eps','png','jpg','heic','gif','tiff', 'mov'):
             badform = 'Unknown export format "%s"'%format
             raise DeviceError(badform)
 
