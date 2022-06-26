4f8ad1b7c8f6910c99b16d6784d5150a85b0d8b3
Christian Swinehart <drafting@samizdat.co>
2022-06-26 14:27:49 -0400

    use venv rather than build dir when running in-place

--- plotdevice/run/__init__.py
+++ plotdevice/run/__init__.py
@@ -1,14 +1,30 @@
 import sys, site
 from os.path import abspath, dirname, relpath, exists, join
+from subprocess import call, getoutput
 
 try:
     # test the sys.path by attempting to load a PyObjC submodule...
     import objc
 except ImportError:
-    # ...but if run from the sdist, the PyObjC site-dir will be in setup.py's build directory
-    objc_dir = abspath(join(dirname(__file__), '../../build/lib/plotdevice/lib/PyObjC'))
-    if exists(objc_dir):
-        site.addsitedir(objc_dir)
+    deps_dir = join(dirname(__file__), '../../app/deps')
+    if exists(deps_dir):
+        # if run from the sdist, install pyobjc et al. in a venv at app/deps/local
+        venv_dir = join(deps_dir, 'local')
+        if not exists(venv_dir):
+            import importlib.util
+            spec = importlib.util.spec_from_file_location("setup", join(dirname(__file__), '../../setup.py'))
+            setup = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(setup)
+
+            import venv
+            venv.create(venv_dir, symlinks=True, with_pip=True)
+            PIP = '%s/bin/pip3' % venv_dir
+            call([PIP, 'install', '--upgrade', 'pip'])
+            call([PIP, '--isolated', 'install', *setup.config['install_requires']])
+
+        # use the venv's site directory
+        site_path = getoutput('%s/bin/python3 -c "import site; print(site.getsitepackages()[0])"' % venv_dir)
+        site.addsitedir(site_path)
         import objc
     else:
         from pprint import pformat
