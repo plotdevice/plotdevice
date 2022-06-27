5d1c2940973ecc28ebb58aa830b186fc55d628ab
Christian Swinehart <drafting@samizdat.co>
2022-06-27 17:49:31 -0400

    make Region hashable

--- plotdevice/gfx/geometry.py
+++ plotdevice/gfx/geometry.py
@@ -281,6 +281,9 @@ class Region(object):
         # allow for assignments like: (x,y), (w,h) = Region()
         return iter([self.origin, self.size])
 
+    def __hash__(self):
+        return hash((getattr(self, attr) for attr in ('x','y','w','h')))
+
     def union(self, *args):
         """Return a new Region which fully encloses the existing Region and the arguments"""
         other = Region(*args)
