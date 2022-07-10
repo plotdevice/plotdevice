ee6076ff9e031791b981649b87ffd8b7a04fdd24
Christian Swinehart <drafting@samizdat.co>
2022-07-10 13:36:25 -0400

    rewrap imports

--- plotdevice/lib/cocoa.py
+++ plotdevice/lib/cocoa.py
@@ -1,16 +1,16 @@
 # all the NSBits and NSPieces
 
 from Quartz import CALayer, CGBitmapContextCreate, CGBitmapContextCreateImage, CGColorCreate, \
-                   CGColorSpaceCreateDeviceRGB, CGContextAddPath, CGContextAddRect, CGContextBeginPath, \
-                   CGContextBeginTransparencyLayer, CGContextBeginTransparencyLayerWithRect, CGContextClearRect, \
-                   CGContextClip, CGContextClipToMask, CGContextDrawPath, CGContextEndTransparencyLayer, \
-                   CGContextEOClip, CGContextRestoreGState, CGContextSaveGState, CGContextSetAlpha, \
-                   CGContextSetBlendMode, CGContextSetFillColorWithColor, CGContextSetLineCap, CGContextSetLineDash, \
-                   CGContextSetLineJoin, CGContextSetLineWidth, CGContextSetStrokeColorWithColor, \
-                   CGDataConsumerCreateWithCFData, CGImageDestinationAddImage, CGImageDestinationCreateWithData, \
-                   CGImageDestinationFinalize, CGImageDestinationSetProperties, CGImageGetBitsPerComponent, \
-                   CGImageGetBitsPerPixel, CGImageGetBytesPerRow, CGImageGetDataProvider, CGImageGetHeight, \
-                   CGImageGetWidth, CGImageMaskCreate, CGPathAddCurveToPoint, CGPathAddLineToPoint, \
+                   CGColorSpaceCreateDeviceCMYK, CGColorSpaceCreateDeviceRGB, CGContextAddPath, CGContextAddRect, \
+                   CGContextBeginPath, CGContextBeginTransparencyLayer, CGContextBeginTransparencyLayerWithRect, \
+                   CGContextClearRect, CGContextClearRect, CGContextClip, CGContextClipToMask, CGContextDrawPath, \
+                   CGContextEndTransparencyLayer, CGContextEOClip, CGContextRestoreGState, CGContextSaveGState, \
+                   CGContextSetAlpha, CGContextSetBlendMode, CGContextSetFillColorWithColor, CGContextSetLineCap, \
+                   CGContextSetLineDash, CGContextSetLineJoin, CGContextSetLineWidth, \
+                   CGContextSetStrokeColorWithColor, CGDataConsumerCreateWithCFData, CGImageDestinationAddImage, \
+                   CGImageDestinationCreateWithData, CGImageDestinationFinalize, CGImageDestinationSetProperties, \
+                   CGImageGetBitsPerComponent, CGImageGetBitsPerPixel, CGImageGetBytesPerRow, CGImageGetDataProvider, \
+                   CGImageGetHeight, CGImageGetWidth, CGImageMaskCreate, CGPathAddCurveToPoint, CGPathAddLineToPoint, \
                    CGPathCloseSubpath, CGPathCreateCopy, CGPathCreateMutable, CGPathMoveToPoint, CGPathRelease, \
                    CGPDFContextBeginPage, CGPDFContextClose, CGPDFContextCreate, CGPDFContextEndPage, CGRectMake, \
                    CGSizeMake, kCGBitmapByteOrder32Host, kCGBlendModeClear, kCGBlendModeColor, kCGBlendModeColorBurn, \
@@ -20,44 +20,43 @@ from Quartz import CALayer, CGBitmapContextCreate, CGBitmapContextCreateImage, C
                    kCGBlendModeLighten, kCGBlendModeLuminosity, kCGBlendModeMultiply, kCGBlendModeNormal, \
                    kCGBlendModeOverlay, kCGBlendModePlusDarker, kCGBlendModePlusLighter, kCGBlendModeSaturation, \
                    kCGBlendModeScreen, kCGBlendModeSoftLight, kCGBlendModeSourceAtop, kCGBlendModeSourceIn, \
-                   kCGBlendModeSourceOut, kCGBlendModeXOR, kCGImageAlphaNoneSkipFirst, \
-                   kCGImageAlphaPremultipliedFirst, kCGImageDestinationLossyCompressionQuality, kCGLineCapButt, \
-                   kCGLineCapRound, kCGLineCapSquare, kCGLineJoinBevel, kCGLineJoinMiter, kCGLineJoinRound, \
-                   kCGPathFill, kCGPathFillStroke, kCGPathStroke, kCIInputImageKey, kCGImageAlphaNone, CGColorSpaceCreateDeviceCMYK, CGContextClearRect, \
-                   kCGImagePropertyDPIWidth, kCGImagePropertyDPIHeight
-
+                   kCGBlendModeSourceOut, kCGBlendModeXOR, kCGImageAlphaNone, kCGImageAlphaNoneSkipFirst, \
+                   kCGImageAlphaPremultipliedFirst, kCGImageDestinationLossyCompressionQuality, \
+                   kCGImagePropertyDPIHeight, kCGImagePropertyDPIWidth, kCGLineCapButt, kCGLineCapRound, \
+                   kCGLineCapSquare, kCGLineJoinBevel, kCGLineJoinMiter, kCGLineJoinRound, kCGPathFill, \
+                   kCGPathFillStroke, kCGPathStroke, kCIInputImageKey
 from AppKit import NSAlert, NSApp, NSAppearance, NSApplication, NSApplicationActivationPolicyAccessory, \
-                   NSBackingStoreBuffered, NSBeep, NSBezierPath, NSBitmapImageRep, NSBorderlessWindowMask, \
-                   NSButton, NSCenterTextAlignment, NSChangeAutosaved, NSChangeCleared, NSChangeDone, \
-                   NSChangeReadOtherContents, NSChangeRedone, NSChangeUndone, NSClipView, \
-                   NSClosePathBezierPathElement, NSColor, NSColorSpace, NSCompositeCopy, \
-                   NSCompositeSourceOver, NSContentsCellMask, NSCriticalAlertStyle, NSCursor, \
+                   NSBackingStoreBuffered, NSBeep, NSBezierPath, NSBitmapImageRep, NSBorderlessWindowMask, NSButton, \
+                   NSCenterTextAlignment, NSChangeAutosaved, NSChangeCleared, NSChangeDone, NSChangeReadOtherContents, \
+                   NSChangeRedone, NSChangeUndone, NSClipView, NSClosePathBezierPathElement, NSColor, NSColorSpace, \
+                   NSCompositeCopy, NSCompositeSourceOver, NSContentsCellMask, NSCriticalAlertStyle, NSCursor, \
                    NSCurveToBezierPathElement, NSDeviceCMYKColorSpace, NSDeviceRGBColorSpace, NSDocument, \
-                   NSDocumentController, NSFindPboard, NSFixedPitchFontMask, NSFocusRingTypeExterior, \
-                   NSFont, NSFontDescriptor, NSFontManager, NSForegroundColorAttributeName, NSGIFFileType, \
-                   NSGradient, NSGraphicsContext, NSGraphiteControlTint, NSImage, NSImageCacheNever, \
-                   NSImageCompressionFactor, NSImageInterpolationHigh, NSItalicFontMask, NSJPEGFileType, \
-                   NSJustifiedTextAlignment, NSKeyValueObservingOptionNew, NSLayoutManager, NSLeftTextAlignment, \
+                   NSDocumentController, NSFindPboard, NSFixedPitchFontMask, NSFocusRingTypeExterior, NSFont, \
+                   NSFontDescriptor, NSFontManager, NSForegroundColorAttributeName, NSGIFFileType, NSGradient, \
+                   NSGraphicsContext, NSGraphiteControlTint, NSImage, NSImageCacheNever, NSImageCompressionFactor, \
+                   NSImageInterpolationHigh, NSItalicFontMask, NSJPEGFileType, NSJustifiedTextAlignment, \
+                   NSKernAttributeName, NSKeyValueObservingOptionNew, NSLayoutManager, NSLeftTextAlignment, \
                    NSLineBreakByWordWrapping, NSLineToBezierPathElement, NSMenu, NSMenuItem, NSMiniControlSize, \
-                   NSMoveToBezierPathElement, NSMutableParagraphStyle, NSNib, NSOffState, NSOnState, NSPNGFileType, \
-                   NSParagraphStyleAttributeName, NSPasteboard, NSPasteboardTypePDF, NSPasteboardURLReadingContentsConformToTypesKey, \
-                   NSPasteboardURLReadingFileURLsOnlyKey, NSPasteboardTypeTIFF, NSPrintOperation, NSRectFill, \
-                   NSRectFillUsingOperation, NSResponder, NSRightTextAlignment, NSSavePanel, NSScreen, NSShadow, \
-                   NSSlider, NSSmallControlSize, NSSplitView, NSStringPboardType, NSSwitch, \
-                   NSTIFFFileType, NSTextContainer, NSTextField, NSTextFinder, NSTextStorage, NSTextView, \
-                   NSToolbarItem, NSTrackingActiveInActiveApp, NSTrackingArea, NSTrackingMouseEnteredAndExited, \
-                   NSUnboldFontMask, NSUnitalicFontMask, NSUnionRect, NSView, NSViewMinXMargin, NSViewWidthSizable,  \
-                   NSViewFrameDidChangeNotification, NSWindow, NSWindowBackingLocationVideoMemory, \
-                   NSWindowController, NSWindowTabbingModeAutomatic, NSWindowTabbingModePreferred, NSWorkspace, NSKernAttributeName, NSTrackingArea, NSTrackingMouseMoved, NSTrackingActiveInKeyWindow
-from Foundation import CIAffineTransform, CIColorMatrix, CIContext, CIFilter, CIImage, \
-                   CIVector, Foundation, NO, NSAffineTransform, NSAffineTransformStruct, \
-                   NSAttributedString, NSAutoreleasePool, NSBundle, NSData, NSDate, NSDateFormatter, \
-                   NSFileCoordinator, NSFileHandle, NSFileHandleDataAvailableNotification, NSIntersectionRange, \
-                   NSHeight, NSInsetRect, NSIntersectionRect, NSLocale, NSLog, NSMacOSRomanStringEncoding, \
-                   NSMakeRange, NSMidX, NSMidY, NSMutableAttributedString, NSMutableData, NSNotificationCenter, NSObject,\
-                   NSOffsetRect, NSOperationQueue, NSPoint, NSRect, NSRectFromString, NSSelectorFromString, \
-                   NSSize, NSString, NSStringFromRect, NSTimeZone, NSTimer, NSURL, NSUserDefaults, \
-                   NSUTF8StringEncoding, NSWidth
+                   NSMoveToBezierPathElement, NSMutableParagraphStyle, NSNib, NSOffState, NSOnState, \
+                   NSParagraphStyleAttributeName, NSPasteboard, NSPasteboardTypePDF, NSPasteboardTypeTIFF, \
+                   NSPasteboardURLReadingContentsConformToTypesKey, NSPasteboardURLReadingFileURLsOnlyKey, \
+                   NSPNGFileType, NSPrintOperation, NSRectFill, NSRectFillUsingOperation, NSResponder, \
+                   NSRightTextAlignment, NSSavePanel, NSScreen, NSShadow, NSSlider, NSSmallControlSize, NSSplitView, \
+                   NSStringPboardType, NSSwitch, NSTextContainer, NSTextField, NSTextFinder, NSTextStorage, \
+                   NSTextView, NSTIFFFileType, NSToolbarItem, NSTrackingActiveInActiveApp, \
+                   NSTrackingActiveInKeyWindow, NSTrackingArea, NSTrackingArea, NSTrackingMouseEnteredAndExited, \
+                   NSTrackingMouseMoved, NSUnboldFontMask, NSUnionRect, NSUnitalicFontMask, NSView, \
+                   NSViewFrameDidChangeNotification, NSViewMinXMargin, NSViewWidthSizable, NSWindow, \
+                   NSWindowBackingLocationVideoMemory, NSWindowController, NSWindowTabbingModeAutomatic, \
+                   NSWindowTabbingModePreferred, NSWorkspace
+from Foundation import CIAffineTransform, CIColorMatrix, CIContext, CIFilter, CIImage, CIVector, Foundation, NO, \
+                       NSAffineTransform, NSAffineTransformStruct, NSAttributedString, NSAutoreleasePool, NSBundle, \
+                       NSData, NSDate, NSDateFormatter, NSFileCoordinator, NSFileHandle, \
+                       NSFileHandleDataAvailableNotification, NSHeight, NSInsetRect, NSIntersectionRange, \
+                       NSIntersectionRect, NSLocale, NSLog, NSMacOSRomanStringEncoding, NSMakeRange, NSMidX, NSMidY, \
+                       NSMutableAttributedString, NSMutableData, NSNotificationCenter, NSObject, NSOffsetRect, \
+                       NSOperationQueue, NSPoint, NSRect, NSRectFromString, NSSelectorFromString, NSSize, NSString, \
+                       NSStringFromRect, NSTimer, NSTimeZone, NSURL, NSUserDefaults, NSUTF8StringEncoding, NSWidth
 from LaunchServices import kUTTypePNG, kUTTypeJPEG, kUTTypeGIF, kUTTypeTIFF
 from WebKit import WebView
 from objc import IBOutlet, IBAction
